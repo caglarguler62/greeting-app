b85e6f173169cd5a09823b672c0b0c79ea761117
Caglar Guler <caglarguler626262@gmail.com>
2021-01-02 16:02:40 -0500

    create greating class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// class Header extends React.Component {
+//   render(){
+//     return (
+//       <div>
+//         <h1>Hello Worls</h1>
+//         <p>this is parent/child-sibling app</p>
+//       </div>
+//     )
+//   }
+// }
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {userName: 'Guler'};
+  //   this.changeName = this.changeName.bind(this)
+  // }
+  // changeName(newName){
+  //   this.setState = ({userName: newName})
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>
+            <h1>Hello {this.state.userName}</h1>
+            <label htmlFor="user_name">enter your name</label><br/>
+            <input id="user_name"type="text"/>
+          </div>
+        )
+  }
 }
 
 export default App;
